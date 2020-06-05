19aa7188d3ff8a28edb29165b402a9363c944b69
Vardan Bansal <vardan.bansal@harness.io>
2020-06-06 03:27:43 +0530

    Integrated presentation links

--- src/sections/Talks.js
+++ src/sections/Talks.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Section from '../components/Section';
 import Triangle from '../components/Triangle';
+import { StaticQuery, graphql } from 'gatsby';
+import { CardContainer, Card } from '../components/Card';
+import Fade from 'react-reveal/Fade';
 
 const Background = () => (
     <div>
@@ -30,7 +33,30 @@ const Background = () => (
 
 const Talks = () => (
     <Section.Container id="about" Background={Background}>
-        <Section.Header name="Talks" icon="🙋‍♂️" label="person" />
+        <Section.Header name="Talks" icon="🎙" label="person" />
+        <StaticQuery
+      query={graphql`
+      query TalksQuery {
+        allContentfulTalks {
+        nodes {
+            topic
+            presentationUrl
+                }
+        }
+      }
+      `}
+      render={({allContentfulTalks}) => (
+        <CardContainer minWidth="350px">
+          {allContentfulTalks.nodes.map((p, i) => {
+              const presentationUrl = p?.presentationUrl
+              console.log(presentationUrl)
+            return <Fade bottom delay={i * 200} key={presentationUrl}>
+                <a href={presentationUrl} target="_blank" rel="noopener noreferrer">{p.topic}</a>
+            </Fade>
+      })}
+        </CardContainer>
+      )}
+    />
     </Section.Container>
 );
 
