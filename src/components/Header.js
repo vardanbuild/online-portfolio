2076f25bca1f9c68deef1ed7b958db461cd5306a
Vardan Bansal <vardan.bansal@harness.io>
2020-06-06 06:33:43 +0530

    fixed logo import

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { SectionLinks } from 'react-scroll-section';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
-import Logo from './Logo/Logo.png';
+import Logo from './Logo/VBLogo.png';
 
 const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
 
