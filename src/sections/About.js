c1789b635404c6bc62cc193d44f77b2a5163d3ed
Vardan Bansal <vardan.bansal@harness.io>
2020-06-23 16:10:54 +0530

    Refactored about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Box, Image, Flex } from 'rebass/styled-components';
 import { StaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
-import ReactMarkdown from 'react-markdown';
 import Fade from 'react-reveal/Fade';
 import Section from '../components/Section';
 import Triangle from '../components/Triangle';
-import markdownRenderer from '../components/MarkdownRenderer';
 
 const Background = () => (
   <div>
@@ -41,37 +39,58 @@ const ProfilePicture = styled(Image)`
   }
 `;
 
+const CARD_WIDTH = '50px';
+
+const MEDIA_QUERY_SMALL = '@media (max-width: 500px)';
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  margin: 10px 0;
+  width: 100%;
+  width: calc(100% - ${CARD_WIDTH});
+  border: 1px solid #A9A9A9;
+  border-radius: 8px;
+
+  ${MEDIA_QUERY_SMALL} {
+    width: calc(100% - (${CARD_WIDTH} / 2));
+  }
+`;
+
 const About = () => (
   <Section.Container id="about" Background={Background}>
     <Section.Header name="About me" icon="🙋‍♂️" label="person" />
     <StaticQuery
       query={graphql`
-        query AboutMeQuery {
-          contentfulAbout {
-            aboutMe {
-              childMarkdownRemark {
-                rawMarkdownBody
-              }
-            }
-            profile {
-              title
-              image: resize(width: 450, quality: 100) {
-                src
-              }
+      query AboutMeQuery {
+        contentfulAbout {
+          profile {
+            title
+            image: resize(width: 450, quality: 100) {
+              src
             }
           }
         }
+        allContentfulAbout {
+          edges {
+            node {
+              details
+            }
+          }
+        }
+      }
       `}
       render={(data) => {
-        const { aboutMe, profile } = data.contentfulAbout;
+        const { contentfulAbout, allContentfulAbout } = data
+        const { profile } = contentfulAbout;
+        const { details } = allContentfulAbout?.edges[0].node;
         return (
           <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
             <Box width={[1, 1, 4 / 6]} px={[1, 2, 4]}>
               <Fade bottom>
-                <ReactMarkdown
-                  source={aboutMe.childMarkdownRemark.rawMarkdownBody}
-                  renderers={markdownRenderer}
-                />
+                {details.map(detail => <TextContainer>{detail}</TextContainer>)
+                }
               </Fade>
             </Box>
             <Box
